7ea6d8f97d42916f55b77a9e9456079a3c792b5d
fagarra <fagarra@gmail.com>
2024-09-28 10:53:45 -0500

    Cambios actualizados

---
+++ src/main/java/desarrollosoftwareisabados/springbootthymeleafcrud/repository/TutorialRepository.java
@@ -0,0 +1,17 @@
+package desarrollosoftwareisabados.springbootthymeleafcrud.repository;
+import java.util.List;
+
+import desarrollosoftwareisabados.springbootthymeleafcrud.entity.Tutorial;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Integer>{
+    List<Tutorial> findByTitleContainingIgnoreCase(String keyword);
+
+    @Query("UPDATE Tutorial t SET t.published = :published WHERE t.id = :id")
+    @Modifying
+    public void updatePublishedStatus(Integer id, boolean published);
+}
