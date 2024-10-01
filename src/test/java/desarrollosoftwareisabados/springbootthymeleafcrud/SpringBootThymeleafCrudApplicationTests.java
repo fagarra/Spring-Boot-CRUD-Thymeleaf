c403954fde7443a90d3a9b21d410bc6a67989de2
fagarra <fagarra@gmail.com>
2024-09-30 19:31:11 -0500

    Initial commit

---
+++ src/test/java/desarrollosoftwareisabados/springbootthymeleafcrud/SpringBootThymeleafCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package desarrollosoftwareisabados.springbootthymeleafcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
